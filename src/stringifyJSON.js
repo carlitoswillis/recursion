e4898601f19f32b0ef2797e8dae0cdbb75f63c7e
carlitoswillis <59816053+carlitoswillis@users.noreply.github.com>
2020-03-06 00:29:06 -0800

    should stringify accurately
    lots of looping involved, im not sure its a good or bad thing...

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,59 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+
+
   // your code goes here
-};
+  var typeobj = typeof obj;
+
+  if (typeobj === "number" || typeobj === "boolean") {
+  	return (obj + ""); 
+  } else if (typeobj === "string") {
+  	return '"' + obj + '"';
+  } else if (obj === null) {
+  	return "null";
+  } else if (typeobj === "function") {
+  	return "";
+  } else if (obj === undefined) {
+  	return "";
+  } else if (Array.isArray(obj)) {
+  	var arrStrReturn = "";
+  	if (obj.length === 0) {
+  	} else {
+  		for (var element of obj) {
+  			if (element !== obj[obj.length-1]) {
+  				arrStrReturn = arrStrReturn + stringifyJSON(element) + ","
+  			} else {
+  				arrStrReturn = arrStrReturn + stringifyJSON(element);
+  			}	
+  		}
+  	}
+  	// else if (obj.length === 1) {
+  	// 	arrStrReturn = stringifyJSON(obj[0]);
+  	// } else {
+  	// 	arrStrReturn = stringifyJSON(obj[0]) + ","+ stringifyJSON(obj.slice(1)) ;
+  	// }
+  	return '[' + arrStrReturn + ']'
+  } else {
+
+    var keys = Object.keys(obj);
+  	var arrObjReturn = "";
+  	if (Object.keys(obj).length === 0) {
+  		return "{}";
+  	} else {
+  		for (var key of keys) {
+  			if (stringifyJSON(obj[key]) === '') {
+  			} else if (key !== keys[keys.length-1]) {
+  				arrObjReturn = arrObjReturn + stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ","
+  			} else {
+  				arrObjReturn = arrObjReturn + stringifyJSON(key) + ":" + stringifyJSON(obj[key]);
+  			}	
+  		}
+  	}
+  	
+  	return '{' + arrObjReturn + '}'
+
+  }
+
+
+}
