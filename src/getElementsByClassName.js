fbfb68fdfc127695f2f65638594a7eaf8af75111
carlitoswillis <59816053+carlitoswillis@users.noreply.github.com>
2020-03-07 13:58:55 -0800

    getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,34 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+
+  var body = document.body;
+  var classNameList = [];
+  
+  function getElementsHelper (element) {
+
+  	if (element === undefined || element.className === undefined || element.classList === undefined) {
+  		return;
+  	}
+
+  	if (element.className === className || element.classList.contains(className)) {
+  		classNameList.push(element);
+  	}
+
+	for (var item of element.childNodes) {
+
+		if (item === undefined || item.classList === undefined) {
+
+		} else {
+			getElementsHelper(item);
+		}
+
+	  }
+
+  }
+
+  getElementsHelper(body);
+
+  return classNameList;
+
 };
